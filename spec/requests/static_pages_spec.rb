ab63e432c2538dc76a4222f293a80d5d54d8cd6d
davidparracatalan <davidparracatalan@gmail.com>
2012-04-26 00:47:02 +0200

    Fixing title when no custom title exists

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-
   describe "Home page" do
     it "Should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text=>"Sample App")
     end
 
-    it "Should have the title 'Home'" do
+    it "Should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text=>"#{base_title} | Home")
+      page.should have_selector('title', :text=>"Ruby on Rails Tutorial Sample App")
+    end
+
+    it "Should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text=>"| Home")
     end
   end
 
@@ -25,7 +28,7 @@ describe "StaticPages" do
     it "Should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-                             :text=>"#{base_title} | Help")
+                             :text=>"Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -38,7 +41,7 @@ describe "StaticPages" do
     it "Should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                             :text=>"#{base_title} | About Us")
+                             :text=>"Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -50,7 +53,7 @@ describe "StaticPages" do
     it "Should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title', 
-                             :text=>"#{base_title} | Contact")
+                             :text=>"Ruby on Rails Tutorial Sample App")
     end
     
   end
