4c602c3c287d977710f09822a019cc9bbe1f9fa6
davidparracatalan <davidparracatalan@gmail.com>
2012-04-28 08:50:56 +0200

    Some comments added in ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
 	def full_title (page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
-			base_title
+			base_title                      #implicit return
 		else
-			"#{base_title} | #{page_title}"
+			"#{base_title} | #{page_title}" #String interpolation
 		end
 	end
 end
